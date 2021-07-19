6ab78372aed8f6a94e949ac56800a5d135cfde67
Henrique de Almeida <oedudealmeida@gmail.com>
2021-07-19 19:32:10 -0300

    Início do Alurakut

---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+
+function ProfileSidebar(propriedades) {
+  console.log(propriedades);
+  return (
+    <Box>
+      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '10px' }} />
+    </Box>
+  )
+}
+
+export default function Home() {
+  const githubUser = 'henriquedealmeida';
+  const amigos = ['omariosouto', 'peas', 'marcobrunodev', 'felipefialho']
+
+  return (
+    <>
+      <AlurakutMenu/>
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser}/>
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1>
+              Bem-vindo(a)
+            </h1>
+
+            <OrkutNostalgicIconSet/>
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({amigos.length})
+            </h2>
+            
+            <ul>
+              {amigos.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                    <img src={`https://github.com/${itemAtual}.png`} />
+                    <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>
+            Comunidades
+          </Box>
+        </div>
+      </MainGrid>
+    </>
+  )
+}
